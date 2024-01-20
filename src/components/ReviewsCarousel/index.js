cf00b8e55de4df2d5b70ca3c3b817dc1c645424c
nithintechie123 <nithingorintala212121@zohomail.ingit config --global user.email nithingorintala212121@zohomail.in>
2024-01-20 14:07:25 +0000

    review app built using react js

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,80 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {activeReviewIndex: 0}
+
+  onClickLeftArrow = () => {
+    const {activeReviewIndex} = this.state
+
+    if (activeReviewIndex > 0) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex - 1,
+      }))
+    }
+  }
+
+  renderActiveReview = updatedReview => {
+    const {imgUrl, username, companyName, description} = updatedReview
+
+    return (
+      <div className="profile-container">
+        <img src={imgUrl} alt={username} className="profile-image" />
+        <p className="user-name">{username}</p>
+        <p className="company-name">{companyName}</p>
+        <p className="description">{description}</p>
+      </div>
+    )
+  }
+
+  onClickRightArrow = () => {
+    const {activeReviewIndex} = this.state
+    const {reviewsList} = this.props
+
+    if (activeReviewIndex < reviewsList.length - 1) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex + 1,
+      }))
+    }
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    const {activeReviewIndex} = this.state
+    const currentReviewList = reviewsList[activeReviewIndex]
+
+    return (
+      <div className="app-container">
+        <h1 className="main-heading">Reviews</h1>
+        <div className="carousel-container">
+          <button
+            className="left-arrow"
+            type="button"
+            data-testid="leftArrow"
+            onClick={this.onClickLeftArrow}
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png "
+              alt="left arrow"
+            />
+          </button>
+          {this.renderActiveReview(currentReviewList)}
+          <button
+            className="right-arrow"
+            type="button"
+            data-testid="rightArrow"
+            onClick={this.onClickRightArrow}
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png "
+              alt="right arrow"
+            />
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
